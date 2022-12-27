6ec02862a92aa1fba2a609f293f7fa1e9d059aa5
joanne92324 <homework92324@gmail.com>
2022-12-27 23:29:24 +0800

    make it better

--- index.py
+++ index.py
@@ -41,7 +41,7 @@ def webhook():
         cond =  req.get("queryResult").get("parameters").get("McdonaldQ")
         keyword =  req.get("queryResult").get("parameters").get("any")
         info = "您要查詢食物的" + cond + "，關鍵字是：" + keyword + "\n\n"
-        if (cond == "食物"):
+        if (cond == "品名"):
             collection_ref = db.collection("麥當勞")
             docs = collection_ref.order_by("kcal").get()
             found = False
@@ -53,7 +53,7 @@ def webhook():
                     info += "熱量：" + doc.to_dict()["kcal"] + "\n" 
                     info += "分類：" + doc.to_dict()["meat"] + "\n\n"
             if not found:
-                info += "很抱歉，目前無符合這個關鍵字的相關電影喔"  
+                info += "很抱歉，目前無符合這個關鍵字的相關食物喔"  
     return make_response(jsonify({"fulfillmentText": info}))
 
 if __name__ == "__main__":
