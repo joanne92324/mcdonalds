695b8506c3ebfaacad0cf6cfaa306bfc7f87c161
joanne92324 <homework92324@gmail.com>
2022-12-27 19:12:28 +0800

    make it better

--- index.py
+++ index.py
@@ -36,21 +36,7 @@ def webhook():
     #info = "動作 :" + action+"; 查詢內容 :" + msg
     if(action == "McDetails"):
        Hamburger = req.get("queryResult").get("parameters").get("Hamburger")
-        if(Hamburger == "嫩煎雞腿堡"):
-            Hamburger = "嫩煎雞腿堡"
-        elif(Hamburger == "麥香雞"):
-            Hamburger = "麥香雞(好吃)"
-       info = "您選擇的食物是: " + Hamburger + "相關食物: \n"
-
-       collection_ref = db.collection("麥當勞")
-       docs = collection_ref.get()
-       result = ""
-       for doc in docs:
-        dict = doc.to_dict()
-        if Hamburger in dict["Hamburger"]:
-            result +="品名: " + dict["product"] + "\n"
-            result +="網站: " + dict["hyperlink"] + "\n"
-        info += result
+       info = "您選擇的食物是: " + Hamburger 
     return make_response(jsonify({"fulfillmentText": info}))
 
 #if __name__ == "__main__":
