112a109717393d43cd926c2bc370e4d68b582d59
joanne92324 <homework92324@gmail.com>
2022-12-27 21:49:31 +0800

    make it better

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -34,10 +34,27 @@ def webhook():
     action =  req["queryResult"]["action"]
     #msg =  req["queryResult"]["queryText"]
     #info = "動作 :" + action+"; 查詢內容 :" + msg
-    if(action == "McDetails"):
-       Hamburger = req.get("queryResult").get("parameters").get("Hamburger")
-        info = "您選擇的食物是: " + Hamburger 
+    if (action == "McDetails"):
+        genres =  req.get("queryResult").get("parameters").get("Hamburger")
+        info = "食物類型：" + Hamburger
+    elif (action == "Mc"): 
+        cond =  req.get("queryResult").get("parameters").get("McdonaldQ")
+        keyword =  req.get("queryResult").get("parameters").get("any")
+        info = "您要查詢食物的" + cond + "，關鍵字是：" + keyword + "\n\n"
+        if (cond == "食物"):
+            collection_ref = db.collection("麥當勞")
+            docs = collection_ref.order_by("kcal").get()
+            found = False
+            for doc in docs:
+                if keyword in doc.to_dict()["product"]:
+                    found = True 
+                    info += "品名：" + doc.to_dict()["product"] + "\n" 
+                    info += "食物介紹：" + doc.to_dict()["hyperlink"] + "\n"
+                    info += "熱量：" + doc.to_dict()["kcal"] + "\n" 
+                    info += "分類：" + doc.to_dict()["meat"] + "\n\n"
+            if not found:
+                info += "很抱歉，目前無符合這個關鍵字的相關電影喔"  
     return make_response(jsonify({"fulfillmentText": info}))
 
-#if __name__ == "__main__":
-#   app.run()
+if __name__ == "__main__":
+   app.run()
