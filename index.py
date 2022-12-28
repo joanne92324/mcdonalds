5715ac9f740682863807096082761748e89c0493
joanne92324 <homework92324@gmail.com>
2022-12-28 10:58:09 +0800

    make it better

--- index.py
+++ index.py
@@ -38,6 +38,18 @@ def webhook():
     if (action == "McDetails"):
         Hamburger =  req.get("queryResult").get("parameters").get("Hamburger")
         info = "食物類型：" + Hamburger  
+        collection_ref = db.collection("麥當勞")
+        docs = collection_ref.get()
+        found = False
+        for doc in docs:
+            if Hamburger in doc.to_dict()["product"]:
+                found = True 
+                info += "品名：" + doc.to_dict()["product"] + "\n" 
+                info += "食物介紹：" + doc.to_dict()["hyperlink"] + "\n"
+                info += "熱量：" + doc.to_dict()["kcal"] + "\n" 
+                info += "分類：" + doc.to_dict()["meat"] + "\n\n"
+        if not found:
+            info += "很抱歉，目前無符合這個關鍵字的相關食物喔"
     return make_response(jsonify({"fulfillmentText": info}))
 
 if __name__ == "__main__":
