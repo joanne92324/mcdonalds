d9c2b1ab9e9fa15216fb5ab3db2ebe13dda3adff
joanne92324 <homework92324@gmail.com>
2022-12-28 11:03:12 +0800

    make it better

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>麥當勞資料讀取4</h1>"
+    homepage = "<h1>麥當勞資料讀取5</h1>"
     homepage += "<br><a href=/read>麥當勞</a><br>"
     homepage += "<br><a href=/webhook>麥當勞資料查詢</a><br>"
     return homepage
@@ -45,9 +45,7 @@ def webhook():
             if Hamburger in doc.to_dict()["product"]:
                 found = True 
                 info += "品名：" + doc.to_dict()["product"] + "\n" 
-                info += "食物介紹：" + doc.to_dict()["hyperlink"] + "\n"
                 info += "熱量：" + doc.to_dict()["kcal"] + "\n" 
-                info += "分類：" + doc.to_dict()["meat"] + "\n\n"
         if not found:
             info += "很抱歉，目前無符合這個關鍵字的相關食物喔"
     return make_response(jsonify({"fulfillmentText": info}))
