181756c99bf3b22cd0458e601247be8148f11484
joanne92324 <homework92324@gmail.com>
2022-12-28 23:30:00 +0800

    make it better

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>麥當勞資料讀取6</h1>"
+    homepage = "<h1>麥當勞資料讀取9</h1>"
     homepage += "<br><a href=/read>麥當勞</a><br>"
     homepage += "<br><a href=/webhook>麥當勞資料查詢</a><br>"
     return homepage
@@ -49,7 +49,8 @@ def webhook():
             info += "很抱歉，目前無符合這個關鍵字的相關食物喔"
     elif (action == "Mc"): 
         cond =  req.get("queryResult").get("parameters").get("McdonaldQ")
-        keyword =  req.get("queryResult").get("parameters").get("Hamburger")
+        keyword =  req.get("queryResult").get("parameters").get("any")
+        if (cond == "熱量"):
         collection_ref = db.collection("麥當勞")
         docs = collection_ref.get()
         found = False
