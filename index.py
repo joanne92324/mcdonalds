f51eb46e3f2137ec37403ce2d45ac794e1ff33c1
joanne92324 <homework92324@gmail.com>
2022-12-29 11:12:48 +0800

    make it better

--- index.py
+++ index.py
@@ -50,6 +50,17 @@ def webhook():
                 info += "熱量: " + doc.to_dict()["kcal"] + "\n"
         if not found:
             info = "很抱歉，目前無符合這個關鍵字的相關食物喔"
+    elif (action == "Mc"):
+        McdonaldQ = req.get("queryResult").get("parameters").get("McdonaldQ")
+        collection_ref = db.collection("麥當勞")
+        docs = collection_ref.get()
+        found = False
+        for doc in docs:
+            if McdonaldQ in doc.to_dict()["product"]:
+                found = True
+                info += "熱量: " + doc.to_dict()["kcal"] + "\n"
+        if not found:
+            info += "很抱歉，目前無符合這個關鍵字的相關食物喔"
     return make_response(jsonify({"fulfillmentText": info}))
 
 if __name__ == "__main__":
