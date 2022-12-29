b5e7ea795b4166f7ecd0fef9c1c8088fdaafe971
joanne92324 <homework92324@gmail.com>
2022-12-29 11:22:46 +0800

    make it better

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>麥當勞資料讀取260</h1>"
+    homepage = "<h1>麥當勞資料讀取280</h1>"
     homepage += "<br><a href=/read>麥當勞</a><br>"
     homepage += "<br><a href=/webhook>麥當勞資料查詢</a><br>"
     return homepage
@@ -47,14 +47,15 @@ def webhook():
                 info = "您要查詢" + Hamburger + "的什麼資訊?" + "\n" 
         if not found:
             info = "很抱歉，目前無符合這個關鍵字的相關食物喔"
-    elif (action == "Mc"):
+    elif(action == "Mc"):
         McdonaldQ = req.get("queryResult").get("parameters").get("McdonaldQ")
-        collection_ref = db.collection("麥當勞")
-        docs = collection_ref.get()
-        found = False
-        for doc in docs:
-            if McdonaldQ in doc.to_dict()["product"]:
-                found = True
+        if(McdonaldQ == "熱量"):
+            collection_ref = db.collection("麥當勞")
+            docs = collection_ref.get()
+            found = False
+            for doc in docs:
+                if McdonaldQ in doc.to_dict()["product"]:
+                    found = True
                 info += "熱量: " + doc.to_dict()["kcal"] + "\n"
         if not found:
             info += "很抱歉，目前無符合這個關鍵字的相關食物喔"
