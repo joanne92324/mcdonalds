f6fdb7d682f870cc650f0b89d0f5426517ea78a3
joanne92324 <homework92324@gmail.com>
2022-12-29 21:52:42 +0800

    make it better

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>麥當勞資料讀取1</h1>"
+    homepage = "<h1>麥當勞資料讀取5</h1>"
     homepage += "<br><a href=/read>麥當勞</a><br>"
     homepage += "<br><a href=/webhook>麥當勞資料查詢</a><br>"
     return homepage
@@ -44,23 +44,8 @@ def webhook():
         for doc in docs:
             if Hamburger in doc.to_dict()["product"]:
                 found = True 
-                info = "您要查詢" + Hamburger + "的什麼資訊?" + "\n" 
-        if not found:
-            info = "很抱歉，目前無符合這個關鍵字的相關食物喔"
-    elif(action == "Mc"):
-        Hamburger = req.get("queryResult").get("parameters").get("Hamburger")
-        McdonaldQ = req.get("queryResult").get("parameters").get("McdonaldQ")
-        info = ""
-
-        if(McdonaldQ == "熱量"):
-            collection_ref = db.collection("麥當勞")
-            docs = collection_ref.get()
-            found = False
-            for doc in docs:
-                if Hamburger in doc.to_dict()["product"]:
-                    found = True
-                info += doc.to_dict()["product"]
-                info += "熱量: " + doc.to_dict()["kcal"] + "\n"
+                info += "餐點: " + doc.to_dict()["product"]
+                info += "熱量: " + doc.to_dict()["kcal"] 
         if not found:
             info += "很抱歉，目前無符合這個關鍵字的相關食物喔"
     return make_response(jsonify({"fulfillmentText": info}))
